7f322a95da9e0440b14131d353c2508331d12018
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-04-08 15:43:08 +0300

    chore: set up project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
+
+// import { BooksProvider } from './context/BooksContext';
+// import { NotificationProvider } from '@context/NotificationContext';
+
+function App() {
+	return (
+		// <NotificationProvider>
+		// 	<BooksProvider>
+		<RouterProvider router={router} />
+		// 	</BooksProvider>
+		// </NotificationProvider>
+	);
+}
+
+export default App;
---
+++ src/router/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		// path: '/',
+		// element: <Layout />,
+		// // errorElement: <NotFoundPage />,
+		// children: [
+		// 	{
+		// 		index: true,
+		// 		element: <MainPage />,
+		// 	},
+		// 	{
+		// 		path: '/addBook',
+		// 		element: <AddBook />,
+		// 	},
+		// 	{
+		// 		path: '/editBook/:id',
+		// 		element: <EditBook />,
+		// 	},
+		// 	],
+	},
+]);
+export default router;
